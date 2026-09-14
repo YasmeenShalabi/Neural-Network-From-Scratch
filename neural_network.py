83af6a264d829b8d4eacf36cd8b4830d15c35c1e
Yasmeen Shalabi <105750836+YasmeenShalabi@users.noreply.github.com>
2026-09-14 15:29:52 -0400

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -4,19 +4,25 @@
 import numpy as np #used for matrix operation and mathematical functions
 
 #Sigmoid activation function and its derivative
+#Sigmoid helps a neural network learn non‑linear patterns by squashing numbers into the range 0–1 and making neurons behave like smooth switches
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
+#sigmoid derivative tells NN how much a neuron should adjust its weights during learning->used during backpropagation, where network learns from its mistakes
 def sigmoid_derivative(x):
     return x * (1-x)
 
-#Mean Squared Error(MSE) Loss Function to measure model performance
+#Mean Squared Error(MSE) Loss Function to measure model performance-> tells NN how wrong its predictions are so it knows how to adjust its weights during training
 def mean_squared_error(y_true, y_pred):
     return np.mean(np.square(y_true - y_pred))
 
 #Basic Neural Network Class
 class BasicNeuralNetwork:
-    #initialize NN with random weights and biases
+    #Initialize NN with random weights and biases
+    
+    #Weights tell the NN how strongly each input should influence a neuron’s output
+    #Bias is an extra number the neuron adds so it doesn’t always start at zero; lets the neuron shift its starting point so it doesn’t depend only on the inputs
+    #Hidden layer-> where NN learns patterns that are not obvious from the raw input
     def __init__(self, input_size, hidden_size, output_size):
         self.weights_input_hidden = np.random.randn(input_size, hidden_size) #weights btw input layer and hidden layer
         self.weights_hidden_output = np.random.randn(hidden_size, output_size) #weights btw hidden layer and output layer
@@ -35,7 +41,7 @@ class BasicNeuralNetwork:
     def backward(self, X, y, output, learning_rate):
         output_error = y - output #difference btw actual output(y) and predicted output
         output_delta = output_error * sigmoid_derivative(output) #adjustments for output layer based on error and derivative of sigmoid function
-        hidden_error= np.dot(output_delta, self.weights_hidden_output.T) #error for hidden layer, propogated from output layer
+        hidden_error= np.dot(output_delta, self.weights_hidden_output.T) #error for hidden layer, propogated from output layer. Takes the error from the output layer and sends it backward through the output weights (flipped), so each hidden neuron knows how much error it caused
         hidden_delta = hidden_error * sigmoid_derivative(self.hidden_output) #adjustments for hidden layer
         
         self.weights_hidden_output +=np.dot(self.hidden_output.T, output_delta) * learning_rate #update self weights hidden output
